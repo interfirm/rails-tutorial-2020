b98f7d81ea063f8742a90355bad480727f6d18ac
rnasakazu <zer0.26053608@gmail.com>
2020-03-28 10:22:07 +0900

    refactor: 三項演算子を用いる

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,11 +8,7 @@ class Room < ApplicationRecord
   def has_unread_message?(user)
     return false if messages.empty?
 
-    if user.instance_of?(Customer)
-      latest_read_message_id = user.customer_room.latest_read_message_id
-    elsif user.instance_of?(Admin)
-      latest_read_message_id = AdminRoom.find_by(room_id: id, admin_id: user.id).latest_read_message_id
-    end
+    latest_read_message_id = user.instance_of?(Customer) ? user.customer_room.latest_read_message_id : AdminRoom.find_by(room_id: id, admin_id: user.id).latest_read_message_id
 
     return true if latest_read_message_id.nil?
 
